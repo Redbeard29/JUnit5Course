c59e2de09c6c497ba685866c394000b6ed476680
Redbeard29 <ben@benshermanmusic.com>
2022-04-01 15:01:27 -0400

    initial commit

---
+++ src/test/java/FirstJUnitTestCase.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class FirstJUnitTestCase {
+
+    @Test
+    void test(){
+        fail("Not yet implemented");
+    }
+}
